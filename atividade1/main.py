83f3a3814bf5f057e050520d3047233858ad989c
vnn <vinnicius109@gmail.com>
2024-03-15 09:15:50 -0300

    add atividade 1

---
+++ atividade1/main.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+
+TXT_PATH = "data/pulso.txt"
+IDEAL_PULSE = 70
+VARIATION = 0.11
+ACCEPT_RANGE = 0.11 * 70
+LOWER_LIMIT = 70 - ACCEPT_RANGE
+UPPER_LIMIT = 70 + ACCEPT_RANGE
+
+
+def extract_data(path):
+    data = []
+    with open(path, "r") as file:
+        for line in file:
+            data_line = line.strip().split("\t")
+            data.append(float(data_line[1]))
+    return data
+
+
+def plot_grafico(inside_range, above_range, below_range):
+   
+    categories = ['Dentro', 'Acima', 'Abaixo']
+    values = [inside_range, above_range, below_range]
+
+    plt.title('Frequência Cardíaca')
+    plt.ylabel('Quantidade de funcionarios')
+
+    plt.bar(categories, values, color=['blue', 'red', 'green'])
+
+    plt.show()
+
+
+def main():
+    data = extract_data(TXT_PATH)
+
+    inside_range = sum(1 for i in data if LOWER_LIMIT <= i <= UPPER_LIMIT)
+    below_range = sum(1 for i in data if i < LOWER_LIMIT)
+    above_range = sum(1 for i in data if i > UPPER_LIMIT)
+
+    plot_grafico(inside_range, above_range, below_range)
+
+if __name__ == "__main__":
+    main()
